05793e3f8a1db5aa6bcfdec7aaaa5439a31ac351
Lai Wei <lai.wei@ibigroup.com>
2020-11-25 22:37:13 -0500

    work on model training but keypoint need to adjust

--- cutomizeDataSet.py
+++ cutomizeDataSet.py
@@ -8,76 +8,77 @@ import cv2
 import random
 
 
-json_dir = "/Users/sunny/Documents/OD_Dataset/CustomizedDataSet/validation/annos"
-image_dir = "/Users/sunny/Documents/OD_Dataset/CustomizedDataSet/validation/image"
+json_dir = "/Users/sunny/Documents/OD_Dataset/CustomizedDataSet/test/annos"
+image_dir = "/Users/sunny/Documents/OD_Dataset/CustomizedDataSet/test/image"
 
 def get_clothes_dicts(json_dir):
     dataset_dicts = []
     for annos in os.listdir(json_dir):
-        json_path = json_dir + '/%s'%(annos)
-        with open(json_path) as f:
-            img_anns = json.load(f)
+        if 'json' in annos:
+            json_path = json_dir + '/%s'%(annos)
+            with open(json_path) as f:
+                img_anns = json.load(f)
 
-        record = {}
-        # add general info
-        image_filename = image_dir + '/%s.jpg'%(annos[:-5])
-        record["file_name"] = image_filename
-        record["image_id"] = '%s'%(annos[:-5])
+            record = {}
+            # add general info
+            image_filename = image_dir + '/%s.jpg'%(annos[:-5])
+            record["file_name"] = image_filename
+            record["image_id"] = '%s'%(annos[:-5])
 
-        height, width = cv2.imread(image_filename).shape[:2]
-        record["height"] = height
-        record["width"] = width
+            height, width = cv2.imread(image_filename).shape[:2]
+            record["height"] = height
+            record["width"] = width
 
-        objs = []
-        for key,anno in img_anns.items():
-            if "item" in key:
-                bbox = anno['bounding_box']
-                segmentation = anno['segmentation']
+            objs = []
+            for key,anno in img_anns.items():
+                if "item" in key:
+                    bbox = anno['bounding_box']
+                    segmentation = anno['segmentation']
 
-                # the index starts from 0
-                category_id = anno['category_id'] - 1
+                    # the index starts from 0
+                    category_id = anno['category_id'] - 1
 
 
-                obj = {
-                    "bbox":bbox,
-                    "bbox_mode": BoxMode.XYXY_ABS,
-                    "segmentation":segmentation,
-                    "category_id":category_id,
+                    obj = {
+                        "bbox":bbox,
+                        "bbox_mode": BoxMode.XYXY_ABS,
+                        "segmentation":segmentation,
+                        "category_id":category_id,
 
-                }
-                objs.append(obj)
-            record["annotations"] = objs
-        dataset_dicts.append(record)
+                    }
+                    objs.append(obj)
+                record["annotations"] = objs
+            dataset_dicts.append(record)
 
     return dataset_dicts
 
 # dataset = get_clothes_dicts(json_dir)
 #
-# json_name = 'clothes.json'
+# json_name = 'clothes_dataset_test.json'
 # with open(json_name, 'w') as f:
 #   json.dump(dataset, f)
 
-
 # register new dataset to the detectron2
-for d in ["train", "val"]:
-    DatasetCatalog.register("clothes_" + d, lambda d = d: get_clothes_dicts(json_dir))
-    MetadataCatalog.get("clothes_" + d).set(thing_classes=["short_sleeved_shirt", "long_sleeved_shirt",
-                                                           "short_sleeved_outwear", "long_sleeved_outwear",
-                                                           "vest", "sling", "shorts", "trousers", "skirt",
-                                                           "short_sleeved_dress", "long_sleeved_dress",
-                                                           "vest_dress", "sling_dress"])
-clothes_metadata = MetadataCatalog.get("clothes_train")
+# for d in ["train", "val"]:
+#     DatasetCatalog.register("clothes_" + d, lambda d = d: get_clothes_dicts(json_dir))
+#     MetadataCatalog.get("clothes_" + d).set(thing_classes=["short_sleeved_shirt", "long_sleeved_shirt",
+#                                                            "short_sleeved_outwear", "long_sleeved_outwear",
+#                                                            "vest", "sling", "shorts", "trousers", "skirt",
+#                                                            "short_sleeved_dress", "long_sleeved_dress",
+#                                                            "vest_dress", "sling_dress"])
+# clothes_metadata = MetadataCatalog.get("clothes_train")
 
 
 # visualize to verify register dataset
-dataset_dicts = get_clothes_dicts(json_dir)
-for d in random.sample(dataset_dicts, 1):
-    img = cv2.imread(d["file_name"])
+# dataset_dicts = get_clothes_dicts(json_dir)
+# for d in random.sample(dataset_dicts, 1):
+#     img = cv2.imread(d["file_name"])
+#
+#     visualizer = Visualizer(img[:, :, ::-1], metadata=clothes_metadata, scale=0.5)
+#     out = visualizer.draw_dataset_dict(d)
+#     cv2.namedWindow("enhanced", 0);
+#     cv2.resizeWindow("enhanced", 2000, 800);
+#     cv2.imshow("enhanced", out.get_image()[:, :, ::-1])
+#     cv2.imwrite('verify.png', out.get_image()[:, :, ::-1])
+#     cv2.waitKey()
 
-    visualizer = Visualizer(img[:, :, ::-1], metadata=clothes_metadata, scale=0.5)
-    out = visualizer.draw_dataset_dict(d)
-    cv2.namedWindow("enhanced", 0);
-    cv2.resizeWindow("enhanced", 2000, 800);
-    cv2.imshow("enhanced", out.get_image()[:, :, ::-1])
-    cv2.imwrite('verify.png', out.get_image()[:, :, ::-1])
-    cv2.waitKey()
--- trainData.py
+++ trainData.py
@@ -1,3 +1,4 @@
+
 from detectron2.engine import DefaultTrainer
 from detectron2.utils.logger import setup_logger
 setup_logger()
@@ -14,23 +15,75 @@ from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog, DatasetCatalog
 
+# register new dataset to the detectron2
+from cutomizeDataSet import get_clothes_dicts
+json_dir = "/Users/sunny/Documents/OD_Dataset/CustomizedDataSet/test/annos"
+
+for d in ["train_test"]:
+    DatasetCatalog.register("clothes_" + d, lambda d = d: get_clothes_dicts(json_dir))
+    MetadataCatalog.get("clothes_" + d).set(thing_classes=["short_sleeved_shirt", "long_sleeved_shirt",
+                                                           "short_sleeved_outwear", "long_sleeved_outwear",
+                                                           "vest", "sling", "shorts", "trousers", "skirt",
+                                                           "short_sleeved_dress", "long_sleeved_dress",
+                                                           "vest_dress", "sling_dress"])
+
+
+
+
+
+# # visualize to verify register dataset
+# dataset_dicts = get_clothes_dicts(json_dir)
+# for d in random.sample(dataset_dicts, 1):
+#     img = cv2.imread(d["file_name"])
+#
+#     visualizer = Visualizer(img[:, :, ::-1], metadata=clothes_metadata, scale=0.5)
+#     out = visualizer.draw_dataset_dict(d)
+#     cv2.namedWindow("enhanced", 0);
+#     cv2.resizeWindow("enhanced", 2000, 800);
+#     cv2.imshow("enhanced", out.get_image()[:, :, ::-1])
+#     cv2.imwrite('verify.png', out.get_image()[:, :, ::-1])
+#     cv2.waitKey()
 
 cfg = get_cfg()
+cfg.MODEL.DEVICE = "cpu"
 cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-cfg.DATASETS.TRAIN = ("clothes_train")
+cfg.DATASETS.TRAIN = ("clothes_train_test",)
 cfg.DATASETS.TEST = ()
 cfg.DATALOADER.NUM_WORKERS = 2
 cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
 cfg.SOLVER.IMS_PER_BATCH = 2
 cfg.SOLVER.BASE_LR = 0.00025 #pick a good LR
-cfg.SOLVER.MAX_ITER = 300  #300 iterations
-cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 512
+cfg.SOLVER.MAX_ITER = 2  #300 iterations
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 256
 cfg.MODEL.ROI_HEADS.NUM_CLASSES = 13
 
 os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
 trainer = DefaultTrainer(cfg)
 trainer.resume_or_load(resume=False)
 trainer.train()
+
+
+# Inference should use the config with parameters that are used in training
+# cfg now already contains everything we've set previously. We changed it a little bit for inference:
+cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")  # path to the model we just trained
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7   # set a custom testing threshold
+predictor = DefaultPredictor(cfg)
+
+# # visualize to verify register dataset
+from detectron2.utils.visualizer import ColorMode
+val_dataset = "/Users/sunny/Documents/OD_Dataset/CustomizedDataSet/test/annos"
+dataset_dicts = get_clothes_dicts(val_dataset)
+clothes_metadata = MetadataCatalog.get("clothes_train")
+for d in random.sample(dataset_dicts, 1):
+    img = cv2.imread(d["file_name"])
+    output = predictor(img)
+    visualizer = Visualizer(img[:, :, ::-1], metadata=clothes_metadata, scale=0.5, instance_mode= ColorMode.IMAGE_BW)
+    out = visualizer.draw_instance_predictions(output["instances"].to("cpu"))
+    cv2.namedWindow("enhanced", 0);
+    cv2.resizeWindow("enhanced", 2000, 800);
+    cv2.imshow("enhanced", out.get_image()[:, :, ::-1])
+    cv2.imwrite('validation.png', out.get_image()[:, :, ::-1])
+    cv2.waitKey()
 
 
 
