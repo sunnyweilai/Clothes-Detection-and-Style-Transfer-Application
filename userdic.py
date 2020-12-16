3185de6902127469f29d924af7f00a578ad154b1
Lai Wei <lai.wei@ibigroup.com>
2020-12-16 17:26:40 -0500

    remove unnecessary test files

--- userdic.py
+++
@@ -1,6 +0,0 @@
-json_dir = "/Users/sunny/Documents/OD_Dataset/CustomizedDataSet/test/annos"
-image_dir = "/Users/sunny/Documents/OD_Dataset/CustomizedDataSet/test/image"
-
-test = json_dir.replace('annos',"image")
-
-print(test)
